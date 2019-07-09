ddb060bbe92a1770f2417b867c7a66ef668f1355
Corey Weathers <cweathers@twilio.com>
2019-07-09 16:53:20 +0000

    Adds the images and Settings Components to the project

---
+++ raffler/Components/SettingsComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace raffler.Components
+{
+    public class SettingsComponent : ComponentBase
+    {
+    }
+}
