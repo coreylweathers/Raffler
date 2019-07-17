564026595adba166e55f5100be72870064a7ea13
Corey Weathers <clw895@msn.com>
2019-07-17 09:00:07 -0500

    Adds logic to disable a button

--- raffler/Components/RaffleComponent.cs
+++ raffler/Components/RaffleComponent.cs
@@ -18,13 +18,28 @@ namespace raffler.Components
         [Inject]
         protected IUriHelper UriHelper { get; set; }
 
-        public readonly string RaffleNumber = "(425) 250-9682";
-        public List<RaffleEntry> EntryList { get; set; }
+        protected readonly string RaffleNumber = "(425) 250-9682";
+        protected List<RaffleEntry> EntryList { get; set; }
+
+        protected bool IsStartButtonEnabled { get; set; }
+
+        protected bool IsStopButtonEnabled { get; set; }
+
+        protected bool IsWinnerButtonEnabled { get; set; }
 
         protected override async Task OnInitAsync()
         {
             await InitalizeSignalRAsync();
-            EntryList = (await StorageService.GetRaffleEntriesAsync()).ToList() ?? new List<RaffleEntry>(); 
+            EntryList = (await StorageService.GetRaffleEntriesAsync()).ToList() ?? new List<RaffleEntry>();
+        }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            var raffle = await StorageService.GetCurrentRaffleAsync();
+            IsStartButtonEnabled = raffle.State == RaffleState.NotRunning;
+            IsStopButtonEnabled = !IsStartButtonEnabled;
+            IsWinnerButtonEnabled = raffle.Entries.Any(entry => entry.IsWinner == false);
+            Console.Write($"IsStartButton: {IsStartButtonEnabled}\r\nIsStopButton: {IsStopButtonEnabled}\r\nIsWinnerButton: {IsWinnerButtonEnabled}");
         }
 
         protected async Task UpdateEntryListAsync(RaffleEntry entry)
@@ -33,18 +48,8 @@ namespace raffler.Components
             Console.WriteLine($"{DateTime.Now}: Adding {entry.MessageSid} to the entry list");
             await Task.Run(() => EntryList.Add(entry));
             Console.WriteLine($"{DateTime.Now}: Adding {entry.MessageSid} to the entry list");
+            ToggleEnabledButtons(false, true, true);
             StateHasChanged();
-        }
-
-        private async Task InitalizeSignalRAsync()
-        {
-            var uri = new Uri($"{UriHelper.GetBaseUri()}rafflehub");
-            // in Component Initialization code
-            var connection = HubConnectionBuilder // the injected one from above.
-                    .WithUrl(uri) // The hub URL. If the Hub is hosted on the server where the blazor is hosted, you can just use the relative path.
-                    .Build(); // Build the HubConnection
-            connection.On<RaffleEntry>("AddRaffleEntry", UpdateEntryListAsync);
-            await connection.StartAsync();
         }
 
         protected async Task StartRaffleAsync()
@@ -53,6 +58,7 @@ namespace raffler.Components
             await StorageService.StartRaffleAsync();
             EntryList.Clear();
             Console.WriteLine($"{DateTime.Now}: Started the Raffle");
+            ToggleEnabledButtons(false, true, true);
             StateHasChanged();
         }
 
@@ -61,6 +67,8 @@ namespace raffler.Components
             Console.WriteLine($"{DateTime.Now}: Stopping the Raffle");
             await StorageService.StopRaffleAsync();
             Console.WriteLine($"{DateTime.Now}: Stopped the Raffle");
+            ToggleEnabledButtons(true, false, true);
+            StateHasChanged();
         }
 
         protected async Task ClearRafflesAsync()
@@ -74,7 +82,26 @@ namespace raffler.Components
             var result = await StorageService.SelectRaffleWinnerAsync();
             Console.WriteLine($"{DateTime.Now}: Resulting SID from notifying winner - {result}");
             Console.WriteLine($"{DateTime.Now}: Selected a raffle winner");
+            ToggleEnabledButtons(true, false, false);
             StateHasChanged();
+        }
+
+        private void ToggleEnabledButtons(bool enableStart, bool enableStop, bool enableWinner)
+        {
+            IsStartButtonEnabled = enableStart;
+            IsStopButtonEnabled = enableStop;
+            IsWinnerButtonEnabled = enableWinner;
+        }
+
+        private async Task InitalizeSignalRAsync()
+        {
+            var uri = new Uri($"{UriHelper.GetBaseUri()}rafflehub");
+            // in Component Initialization code
+            var connection = HubConnectionBuilder // the injected one from above.
+                    .WithUrl(uri) // The hub URL. If the Hub is hosted on the server where the blazor is hosted, you can just use the relative path.
+                    .Build(); // Build the HubConnection
+            connection.On<RaffleEntry>("AddRaffleEntry", UpdateEntryListAsync);
+            await connection.StartAsync();
         }
     }
 }
